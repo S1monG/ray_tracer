d7f0ee4541a1ecf8cd77854618789faaba3383c9
S1monG <si5564gu-s@student.lu.se>
2022-06-18 18:40:37 +0200

    iufhdgiufsdb

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::ops::{Add, Sub, Mul};
 
+pub mod ray;
+
 #[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Vec3(f32, f32, f32);
+pub struct Vec3(pub f32, pub f32, pub f32);
 
 impl Add for Vec3 {
     type Output = Vec3;
@@ -28,25 +30,25 @@ impl Mul for Vec3 {
 }
 
 impl Vec3 {
-    fn multiply(self, n: f32) -> Vec3 {
+    pub fn multiply(self, n: f32) -> Vec3 {
         Vec3 (self.0 * n, self.1 * n, self.2 * n)
     }
 
-    fn divide(self, n: f32) -> Vec3 {
+    pub fn divide(self, n: f32) -> Vec3 {
         Vec3 (self.0 / n, self.1 / n, self.2 / n)
     }
 
-    fn dot(self, other: Vec3) -> f32 {
+    pub fn dot(self, other: Vec3) -> f32 {
         (self.0 * other.0) + (self.1 * other.1) + (self.2 * other.2)
     }
 
-    fn cross(self, other: Vec3) -> Vec3 {
+    pub fn cross(self, other: Vec3) -> Vec3 {
         Vec3 (self.1*other.2 - self.2*other.1, 
             self.2*other.0 - self.0*other.2, 
             self.0*other.1 - self.1*other.0)
     }
 
-    fn unit(self) -> Vec3 {
+    pub fn to_unit(self) -> Vec3 {
         self.divide(self.lenght())
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,47 @@
 use image::{RgbImage, Rgb};
+use ray_tracer::ray::Ray;
+use ray_tracer::Vec3;
+
 fn main() {
 
+    // camera setup
 
-    let mut img = RgbImage::new(255, 255);
+    let ratio = 16./9.; 
+    let width = 400;
+    let height = (width as f32 / ratio) as u32;
 
-    for x in 0..255 {
-        for y in 0..255 {
-            img.put_pixel(x, y, Rgb([x as u8, y as u8, 100]));
+    let mut img = RgbImage::new(width, height);
+
+    let focal_length = 100.;
+    let origin = Vec3(width as f32/2., height as f32/2., 0.,);  // middle of the image with z to 0
+
+    // render
+
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            
+            let ray = Ray {
+                origin,
+                direction: Vec3(x as f32, y as f32, focal_length) - origin
+            };
+
+            img.put_pixel(x, y, ray_color(ray));
         }
     }
 
 
     img.save("test.png").unwrap();
 }
+
+fn ray_color(ray: Ray) -> Rgb<u8>{
+
+
+    /* Rgb([
+        (unit_direction.0 * 255.) as u8, 
+        (unit_direction.1 * 255.) as u8, 
+        (unit_direction.2 * 255.) as u8
+    ]) */
+
+    Rgb([ray.direction.0 as u8, ray.direction.1 as u8, 200])
+}
+
